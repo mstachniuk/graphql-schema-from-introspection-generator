f42e0b59d7b738741d2f1ee112c31015a9bb7ed5
Marcin Stachniuk <marcin.stachniuk@collibra.com>
2018-07-09 11:22:39 +0200

    Use JUnit5 Parameterized Tests, read from files

--- src/test/kotlin/io/github/mstachniuk/graphqlfromintrospectiongenerator/GeneratorTest.kt
+++ src/test/kotlin/io/github/mstachniuk/graphqlfromintrospectiongenerator/GeneratorTest.kt
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
+import java.io.File
 import java.util.stream.Stream
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -12,9 +13,8 @@ internal class GeneratorTest {
     @ParameterizedTest
     @MethodSource("sourceProvider")
     fun `should generate schema`(args: Pair<String, String>) {
-        javaClass.getResource(args.first).readText()
-        val input = javaClass.getResource(args.first).readText().trimIndent()
-        val expected = javaClass.getResource(args.second).readText().trimIndent()
+        val input = File(args.first).readText().trimIndent()
+        val expected = File(args.second).readText().trimIndent()
 
         val gen = Generator()
         val result = gen.generate(input)
@@ -23,6 +23,11 @@ internal class GeneratorTest {
     }
 
     fun sourceProvider() : Stream<Pair<String, String>> {
-        return Stream.of(Pair("/testdata/input-1.json", "/testdata/schema-1.graphqls"))
+        val path = System.getProperty("user.dir") + "/src/test/resources/testdata"
+        val file = File(path)
+        val list = file.list { _ , name -> name.matches("input-(\\d*)\\.json$".toRegex()) }
+                .map { name -> Pair("$path/$name", "$path/" + name.replace("input", "schema")
+                        .replace(".json", ".graphqls")) }
+        return list.stream()
     }
 }
