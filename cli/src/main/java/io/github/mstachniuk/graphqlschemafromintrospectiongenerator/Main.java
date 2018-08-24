532a932425699804f4de69145300ea6e017b8ad5
Marcin Stachniuk <mstachniuk@gmail.com>
2018-08-24 17:03:00 +0200

    Add CLI and Fat Jar CLI

--- cli/src/main/java/io/github/mstachniuk/graphqlschemafromintrospectiongenerator/Main.java
+++ cli/src/main/java/io/github/mstachniuk/graphqlschemafromintrospectiongenerator/Main.java
@@ -1,7 +1,39 @@
 package io.github.mstachniuk.graphqlschemafromintrospectiongenerator;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("CLI");
+    public static void main(String[] args) throws IOException {
+        if (args.length != 2) {
+            System.out.println("Application generate GpaphQL Schema from Introspection Query result");
+            System.out.println("Please specify 2 arguments: input and output file, e.g.:");
+            System.out.println("java -jar graphql-schema-from-introspection-generator-cli-X.X.X.jar input.json output.graphqls");
+            System.exit(1);
+        }
+
+        String input = readFile(args[0]);
+
+        String schema = generateSchema(input);
+        saveSchemaToFile(args[1], schema);
+    }
+
+    private static String readFile(String filename) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(filename)), StandardCharsets.UTF_8);
+    }
+
+    private static String generateSchema(String input) {
+        Generator generator = new Generator();
+        return generator.generate(input);
+    }
+
+    private static void saveSchemaToFile(String filename, String schema) throws FileNotFoundException {
+        try (PrintWriter out = new PrintWriter(filename)) {
+            out.println(schema);
+        }
     }
 }
