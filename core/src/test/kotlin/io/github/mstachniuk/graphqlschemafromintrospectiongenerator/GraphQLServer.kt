fea99de1efd454636eb8569cab65f7f1e327836d
Marcin Stachniuk <mstachniuk@gmail.com>
2018-09-11 17:45:32 +0200

    #10 Auto mocking scalars interfaces and unions in tests

--- core/src/test/kotlin/io/github/mstachniuk/graphqlschemafromintrospectiongenerator/GraphQLServer.kt
+++ core/src/test/kotlin/io/github/mstachniuk/graphqlschemafromintrospectiongenerator/GraphQLServer.kt
@@ -1,9 +1,17 @@
 package io.github.mstachniuk.graphqlschemafromintrospectiongenerator
 
 import graphql.GraphQL
+import graphql.language.InterfaceTypeDefinition
+import graphql.language.UnionTypeDefinition
+import graphql.schema.Coercing
+import graphql.schema.GraphQLObjectType
+import graphql.schema.GraphQLScalarType
+import graphql.schema.TypeResolver
 import graphql.schema.idl.RuntimeWiring
 import graphql.schema.idl.SchemaGenerator
 import graphql.schema.idl.SchemaParser
+import graphql.schema.idl.TypeDefinitionRegistry
+import graphql.schema.idl.TypeRuntimeWiring
 import org.json.JSONObject
 
 class GraphQLServer {
@@ -100,14 +108,53 @@ class GraphQLServer {
                 " }\n" +
                 "}"
 
+        val defaultScalars = listOf("Int", "Float", "String", "Boolean", "ID", "Long", "BigInteger", "BigDecimal",
+                "Short", "Char")
+
         fun createGraphQLServer(schemaText: String): GraphQLSerer {
             val schemaParser = SchemaParser()
             val registry = schemaParser.parse(schemaText)
             val schemaGenerator = SchemaGenerator()
-            val runtimeWiring = RuntimeWiring.newRuntimeWiring().build()
+            val runtimeWiring = createRuntimeWiring(registry)
             val schema = schemaGenerator.makeExecutableSchema(registry, runtimeWiring)
             val graphql = GraphQL.newGraphQL(schema).build()
             return GraphQLSerer(graphql)
+        }
+
+        private fun createRuntimeWiring(registry: TypeDefinitionRegistry): RuntimeWiring {
+            val wiring = RuntimeWiring.newRuntimeWiring()
+            registry.scalars()
+                    .filterKeys { key -> !defaultScalars.contains(key) }
+                    .forEach { wiring.scalar(dummyScalar(it.key)) }
+            val interfaceTypes = registry.getTypes(InterfaceTypeDefinition::class.java)
+            interfaceTypes.forEach {
+                wiring.type(TypeRuntimeWiring.newTypeWiring(it.name).typeResolver(defaultTypeResolver()))
+            }
+            val unionTypes = registry.getTypes(UnionTypeDefinition::class.java)
+            unionTypes.forEach {
+                wiring.type(TypeRuntimeWiring.newTypeWiring(it.name).typeResolver(defaultTypeResolver()))
+            }
+            return wiring.build()
+        }
+
+        private fun defaultTypeResolver(): TypeResolver {
+            return TypeResolver { GraphQLObjectType.newObject().build(); }
+        }
+
+        private fun dummyScalar(name: String): GraphQLScalarType {
+            return GraphQLScalarType(name, "desc", object : Coercing<Any, Any> {
+                override fun parseValue(input: Any?): Any {
+                    return ""
+                }
+
+                override fun parseLiteral(input: Any?): Any {
+                    return ""
+                }
+
+                override fun serialize(dataFetcherResult: Any?): Any {
+                    return ""
+                }
+            })
         }
     }
 
